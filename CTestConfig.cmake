b65821d90376e60f70e77939015f463819b98abb
Adam Comley <adam@novators.net>
2016-04-17 08:32:37 -0500

    Test Drop Site

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -9,7 +9,7 @@
 set(CTEST_PROJECT_NAME "libsqrl")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
-set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_METHOD "https")
 set(CTEST_DROP_SITE "test.sqrlid.com")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=libsqrl")
 set(CTEST_DROP_SITE_CDASH TRUE)
