180f27c09d4f4bcef080c58f5b2b63af70f9af15
Adam Comley <adam@novators.net>
2016-08-09 13:19:13 -0500

    Clean up SqrlBigInt

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# libsqrl 0.3.16222.26
+# libsqrl 0.3.16222.51
 
 A SQRL Authentication Library
 
--- src/SqrlBigInt.h
+++ src/SqrlBigInt.h
@@ -24,11 +24,11 @@ namespace libsqrl
         /// <summary>Removes trailing zeros.</summary>
         void stripTrailingZeros() {
             while( this->length() ) {
-                char c = this->popc_back(); // Remove the last byte from buffer.
-                if( c != 0 ) {              // If it is not zero,
-                    this->push_back( c );   // put it back,
-                    break;                  // and end.
-                }                           // Otherwise, discard it and move to previous byte in buffer.
+                char c = this->popc_back();            // Remove the last byte from buffer.
+                if( c != 0 ) {                         // If it is not zero,
+                    this->push_back( c );              // put it back,
+                    break;                             // and end.
+                }                                      // Otherwise, discard it and repeat.
             }
         }
 
@@ -43,26 +43,25 @@ namespace libsqrl
         /// <param name="operand">The byte to add.</param>
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         void add( uint8_t operand ) {
-            if( this->length() == 0 ) {            // If the buffer is empty,
-                this->append( (char)operand, 1 );  // just store the operand.
+            if( this->length() == 0 ) {                // If the buffer is empty,
+                this->append( (char)operand, 1 );      // just store the operand.
                 return;
             }
-            uint16_t carry = 0;                    // Stores the carry byte.
-            uint16_t t = operand;                  // Temporary variable, initialized with operand.
-            uint8_t *it = this->dend();            // Start *after* the last byte in the buffer,
-            uint8_t *end = this->data();           // and end when the first byte has been processed.
-
+            uint16_t carry = operand;                  // Carry byte, initialized with operand.
+            uint16_t t;                                // Temporary variable.
+            
+            uint8_t *it = this->dend();                // Starting *after* the last byte in the buffer,
+            uint8_t *end = this->data();               // and ending when the first byte has been processed:
             do {
-                it--;                              // Move to the previous byte in buffer.
-                t += *it + carry;                  // Add the byte and any carry value.
-                *it = (uint8_t)t;                  // Store result byte,
-                carry = t >> 8;                    // and carry any overflow to next operation.
-                t = 0;                             // Reset temporary variable for next operation.
-            } while( carry && it != end );         // If we have processed the first byte in buffer,
-                                                   // or we no longer have a carry byte, we're done.
+                it--;                                  // Move to the previous byte in buffer.
+                t = *it + carry;                       // Add the byte and any carry value.
+                *it = (uint8_t)t;                      // Store result byte,
+                carry = t >> 8;                        // and carry any overflow to next operation.
+            } while( carry && it != end );             // If we have processed the first byte in buffer,
+                                                       // or we no longer have a carry byte, we're done.
 
-            if( carry ) {                          // If there is still a carry,
-                this->insert( 0, (uint8_t)carry ); // Store it as a new byte at beginning of buffer.
+            if( carry ) {                              // If there is still a carry,
+                this->insert( 0, (uint8_t)carry );     // Store it as a new byte at beginning of buffer.
             }
         }
 
@@ -72,30 +71,28 @@ namespace libsqrl
         /// <param name="multiplicand">The byte to multiply by.</param>
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         void multiplyBy( uint8_t multiplicand ) {
-            if( this->length() == 0 ) return;             // Multiplying by zero is zero.
-            SqrlString result( this->length() + 1 );      // A buffer to build the result in.
-            uint16_t carry = 0;                           // Digit carried from previous operation
-            uint16_t t;                                   // Temporary variable.
+            if( this->length() == 0 ) return;          // Multiplying by zero is zero.
+            uint16_t t = 0;                            // Temporary variable.
+            
+            uint8_t *it = this->dend();                // Starting *after* the last byte in the buffer,
+            uint8_t *end = this->data();               // and ending when the first byte has been processed:
+            do {
+                it--;                                  // Move to the previous byte in buffer.
+                t += ((uint16_t)*it * multiplicand);   // Multiply the byte by multiplicand, and add any carry.
+                *it = (uint8_t)t;                      // Store the result byte,
+                t = t >> 8;                            // And carry overflow to next operation.
+            } while( it != end );                      // Did we just process the first byte in buffer?
 
-            while( this->length() ) {                     // For each byte,
-                t = (uint16_t)this->popb_back()           // starting from the back,
-                    * multiplicand                        // multiply the byte by the multiplicand,
-                    + carry;                              // and add any carry byte from the previous operation.
-                result.push_back( (uint8_t)(t & 0xFF) );  // Save the result.
-                carry = t >> 8;                           // and the carry.
+            if( t ) {                                  // If we still have a carry,
+                this->insert( 0, (uint8_t)t );         // Insert it as a new byte at beginning of buffer.
             }
-            if( carry ) {                                 // If we have a carry after the last byte,
-                result.push_back( (uint8_t)carry );       // Save it in the result.
-            }
-            result.reverse();                             // Reverse the result buffer,
-            this->append( &result );                      // and store the final result.
         }
 
         ////////////////////////////////////////////////////////////////////////////////////////////////////
         /// <summary>Divide the string by a single byte divisor.</summary>
         ///
         /// <remarks>This function implements long division in reverse, meaning that the SqrlString is
-        /// 		 treated as a number in which the first byte is the least significant.</remarks>
+        ///          treated as a number in which the first byte is the least significant.</remarks>
         /// 
         /// <param name="divisor">The divisor.</param>
         ///
@@ -105,15 +102,15 @@ namespace libsqrl
             if( divisor == 0 ) return 0;               // Cannot divide by zero.
             if( this->length() == 0 ) return divisor;  // Dividing zero by anything is 0 remainder divisor.
 
-            uint8_t *it = this->dend();	               // Start *after* the end of the string.
-            uint8_t *end = this->data();               // End when we've processed the first byte of string.
             uint16_t rem = 0;                          // Holds the remainder of most recent divison.
             uint16_t t;                                // Temporary variable.
 
+            uint8_t *it = this->dend();	               // Starting *after* the end of the string.
+            uint8_t *end = this->data();               // And ending when the first byte has been processed:
             do {
                 it--;                                  // Move to the previous byte.
-                t = (rem << 8) + *it;                  // Take this byte and remainder from previous operation.
-                rem = t % divisor;                     // Calculate new remainder.
+                t = (uint16_t)*it + (rem << 8);        // Add this byte and remainder from previous operation.
+                rem = t % divisor;                     // Calculate new remainder,
                 *it = (uint8_t)(t / divisor);          // and replace the byte with the result of division.
             } while( it != end );                      // Repeat until we've processed the first byte.
 
